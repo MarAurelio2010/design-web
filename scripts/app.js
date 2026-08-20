19227f548cb5de87e09860b459f2934b3471a735
MarAurelio2010 <c.aurelio@escolar.ifrn.edu.br>
2026-08-20 13:51:05 -0300

    mudei o alerta

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
-alert("se você está no site, você gosta de verdura!")
+alert("se você está no site, você gosta de verdura!");
+var nasc=2010;
+let nome="Marco";
+const vivo=true;
+let altura=1.78;
+
+if (vivo){
+    let saudacao="Olá "+ nome +"!";
+    let msg= `Altura ${altura}m| Idade ${2026-nasc}`;
+    alert(saudacao+"\n"+msg);
+}else{
+    alert("RIP")
+}
