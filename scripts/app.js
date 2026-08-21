aa2eaa3c1792f567a2984535fa32fe1d9af088c7
MarAurelio2010 <c.aurelio@escolar.ifrn.edu.br>
2026-08-21 14:15:21 -0300

    eu fiz uma mudança

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,13 @@ var nasc=2010;
 let nome="Marco";
 const viva = true;
 
-function calcIdade(ano){
+function calcIdade(ano=2026){
     let idade=ano-nasc;
     alert(`Dentro de função - Idade ${idade}`);
     return idade;
 }
+
 calcIdade();
-alert(`fora da função: Idade ${idade}`);
-alert(`fora da função: chamando calIdade ${calcIdade}`);
+/* alert(`Fora da função: Idade ${idade}`);
+Erro pois a variavel let não existe fora do escopo dela*/
+alert(`Fora da função: chamando calIdade ${calcIdade(2027)}`);
