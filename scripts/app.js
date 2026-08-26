5f9fbf2d015ad7af72d170ba852c5a5ff9c4a9bb
MarAurelio2010 <c.aurelio@escolar.ifrn.edu.br>
2026-08-26 17:16:06 -0300

    eu usei o var e adicionei informações

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,21 @@ const viva = true;
 
 function calcIdade(ano=2026){
     let idade=ano-nasc;
-    alert(`Dentro de função - Idade ${idade}`);
+    let menor;
+    if (idade<18){
+        menor=true;
+        var podebeber=false;
+    }else{
+        menor=false
+        var podebeber=true;
+    }
+    alert(`${nome}é menor de idade?${menor}\nIdade:${idade}\nPode beber:${podebeber}`)
     return idade;
 }
 
 calcIdade();
+alert(`Pode beber:${podebeber}`)
+alert(`Idade:${idade}`)
 /* alert(`Fora da função: Idade ${idade}`);
 Erro pois a variavel let não existe fora do escopo dela*/
 alert(`Fora da função: chamando calIdade ${calcIdade(2027)}`);
