0b1eabfb52e9b0c4f15df7263045d63e6eb64c50
MarAurelio2010 <c.aurelio@escolar.ifrn.edu.br>
2026-08-26 17:51:37 -0300

    eu criei o app3

---
+++ scripts/app3.js
@@ -0,0 +1,15 @@
+let nasc=2010;
+let nome="Marco";
+let viva=true;
+let altura=1.78;
+let sexo;
+let peso=null;
+let imc="peso"*10;
+
+alert(`Ano de nascimento - tipo: ${typeof(nasc)}`)
+alert(`Nome - tipo: ${typeof(nome)}`)
+alert(`Viva - tipo: ${typeof(viva)}`)
+alert(`Altura - tipo: ${typeof(altura)}`)
+alert(`Sexo - tipo: ${typeof(sexo)}`)
+alert(`Peso - tipo: ${typeof(peso)}`)
+alert(`IMC - tipo: ${typeof(imc)}`)
