96aa27e2b6eca13d6b5c9de430044a5bbf4a9a63
MarAurelio2010 <c.aurelio@escolar.ifrn.edu.br>
2026-09-18 14:30:32 -0300

    fiz a aula 9

---
+++ js/app1.js
@@ -0,0 +1,7 @@
+const amigos = ["Pedro", "Ana", "Clara"];
+tamanho = amigos.length
+
+console.log(`Arranjo ${amigos}`)
+console.log(`Tamanho ${tamanho}`)
+console.log(`Primeiro ${amigos[0]}`)
+console.log(`Ùltimo ${amigos[tam-1]}`)
