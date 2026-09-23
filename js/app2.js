17d0f4e802a54fc2acaafea2f22481829474ab58
MarAurelio2010 <c.aurelio@escolar.ifrn.edu.br>
2026-09-23 16:54:04 -0300

    eu fiz uma mudança

--- js/app2.js
+++ js/app2.js
@@ -1,1 +1,8 @@
 const amigos = []
+const cadastro = document.getElementById("cadastro");
+const nome = cadastro.nome;
+const nasc = cadastro.nasc;
+const whatsapp = cadastro.whatsapp;
+cadastro.addEventListener("submit",function(e){
+    e.preventDefault();
+});
