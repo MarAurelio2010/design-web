9089b9507ca559da09c05b33b40af5ac632e7770
MarAurelio2010 <c.aurelio@escolar.ifrn.edu.br>
2026-09-23 17:23:36 -0300

    eu fiz umas mudanças

--- js/app2.js
+++ js/app2.js
@@ -8,16 +8,14 @@ cadastro.addEventListener("submit",function(e){
     e.preventDefault();
     let item = [nome.value, nasc.value, whatsapp.value]
     amigos.unshift(item);
-    //Limpa o formulário
     cadastro.reset();
-    //Atualiza lista
     exibirLista();
 });
 
 function exibirLista(){
     let itens = "";
     for(let i = 0; i<amigos.length; i++){
-        let item = amigos[i]; //item = [Nome,Nasc,Whatsapp]
+        let item = amigos[i];
         let li = `<li>${item[0]} | ${item[1]} | ${item[2]}`
         itens = itens + li
     }
